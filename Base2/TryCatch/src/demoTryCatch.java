2a47938441d124aff469a996cdc02086949ad0a9
x1ptadkm <x1ptadkm@gmail.com>
2025-05-06 14:31:28 +0700

    SRC

--- Base2/TryCatch/src/demoTryCatch.java
+++ Base2/TryCatch/src/demoTryCatch.java
@@ -1,12 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author pta911
- */
-public class demoTryCatch {
-    
+public class demoTryCatch{
+    public static void main(String[] args){
+        try{
+            // code gây ra nhiều loại ngoại lệ khác nhau
+        } catch(NullPointerException e){
+            System.out.println(e);
+        } catch(ArithmeticException e){
+            // các khối catch khác
+        } 
+        System.out.println("Finished!");
+    }
 }

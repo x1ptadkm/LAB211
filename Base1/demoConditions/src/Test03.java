5f1db037b29ac46aeadb0869577349a775cb4e13
x1ptadkm <x1ptadkm@gmail.com>
2025-05-06 12:58:57 +0700

    SRC

---
+++ Base1/demoConditions/src/Test03.java
@@ -0,0 +1,15 @@
+public class Test03{
+    public static void main(String[] args){
+        int day=7;
+        switch(day){
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Error...");
+        }
+    }
+}

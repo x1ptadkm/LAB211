dbbb2981277ad1974f156404182907314e230a19
x1ptadkm <x1ptadkm@gmail.com>
2025-05-06 13:01:53 +0700

    SRC

--- Base1/demoConditions/src/Test03.java
+++ Base1/demoConditions/src/Test03.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
+
 public class Test03{
     public static void main(String[] args){
-        int day=7;
-        switch(day){
-            case 1 -> System.out.println("Monday");
-            case 2 -> System.out.println("Tuesday");
-            case 3 -> System.out.println("Wednesday");
-            case 4 -> System.out.println("Thursday");
-            case 5 -> System.out.println("Friday");
-            case 6 -> System.out.println("Saturday");
-            case 7 -> System.out.println("Sunday");
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Enter a day: ");
+        int days=inp.nextInt();
+        
+        switch(days){
+            case 2 -> System.out.println("Monday");
+            case 3 -> System.out.println("Tuesday");
+            case 4 -> System.out.println("Wednesday");
+            case 5 -> System.out.println("Thursday");
+            case 6 -> System.out.println("Friday");
+            case 7 -> System.out.println("Saturday");
+            case 8 -> System.out.println("Sunday");
             default -> System.out.println("Error...");
         }
     }

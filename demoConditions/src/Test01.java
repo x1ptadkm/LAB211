b5538692149dd10548bbade565eee9edd4d0ea2d
x1ptadkm <x1ptadkm@gmail.com>
2025-05-06 12:43:32 +0700

    SRC

--- demoConditions/src/Test01.java
+++ demoConditions/src/Test01.java
@@ -1,6 +1,8 @@
 public class Test01{
     public static void main(String[] args){
-        int x=10;
+        int x=3;
         if(x>5) System.out.println("x bigger than 5");
+        else System.out.println("x smaller than 5");
     }
 }
+
